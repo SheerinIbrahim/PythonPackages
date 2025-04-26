2ce6ac641b85805902c04af60e7a9fd2240ef809
Sheerin Ibrahim <48320832+SheerinIbrahim@users.noreply.github.com>
2025-04-26 20:30:08 +1000

    Update setup.py

--- handtrackingmodule/setup.py
+++ handtrackingmodule/setup.py
@@ -8,13 +8,12 @@ setup(
         "opencv-python",
         "mediapipe"
     ],
-    author="Sheerin",
+    author="SheerinIbrahim",
     author_email="sheerinsultana96@gmail.com",
     description="Simple Hand Tracking Module using OpenCV and MediaPipe",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/SheerinSultana/PythonPackages/tree/de9b8ba97dcccdcbcce2244b18f48f963738a173/handtrackingmodule",  # if you have github
-    classifiers=[
+    url="https://github.com/SheerinIbrahim/PythonPackages/tree/b08585c8a453c12566efb51cae02412d45175f7c/handtrackingmodule",   classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
